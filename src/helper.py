d4886439ee9ed420ed288e9826f36db23ec2fdc8
Vatsal Shukla <141351992+Vatsalv19@users.noreply.github.com>
2025-05-19 18:11:16 +0530

    helper.py

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
